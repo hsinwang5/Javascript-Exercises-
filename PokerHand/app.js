2d378cc892f8ad90dc5e89e7c14ce91511bfdfed
Hsin Chih Wang <hsinwang5@gmail.com>
2016-03-23 04:00:44 -0400

    Create app.js

---
+++ PokerHand/app.js
@@ -0,0 +1,85 @@
+//This application takes in an array of 5 objects representing a poker hand, 
+//and returns what type of pair it is. 
+//Straights and flushes are not considered, but can be implemented using Math.max and Math.min
+var main = function () {
+	"use strict";
+	console.log("activated");
+	
+	var hand1 = [
+	{"rank": 3, "suit": "spades"},
+	{"rank": 3, "suit": "diamonds"},
+	{"rank": 3, "suit": "hearts"},
+	{"rank": 7, "suit": "hearts"},
+	{"rank": 3, "suit": "clubs"}
+	];
+
+	var cardRanksArray = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+	var ranks = hand1.map(function (card) {
+		return card.rank;
+	});
+
+	var result = containsNTimes(ranks, 5);
+	console.log(result);
+
+	console.log(pairType(hand1));
+	
+	//function takes 2 arugments: an array and a search term, 
+	//and returns the amount of times that search term appears as a number
+	function containsNTimes (array, search) {
+		//check if argument is array
+		if (array.constructor !== Array) {
+			return alert("not an array");
+		}
+		var count = 0;
+		var tracker = 0;
+		while (array.indexOf(search, tracker) !== -1) {
+			count += 1;
+			tracker = (array.indexOf(search, tracker)) + 1;
+		}
+		return count;
+	}
+
+	//function returns true if the hand contains any pair
+	function pairType (hand) {
+		var pairFound = 0;
+		var tripsFound = 0;
+		var quadsFound = 0;
+		var result = "";
+		var rank1;
+		var rank2;
+		var handArray = hand.map(function (card) {
+			return card.rank;
+		});
+		cardRanksArray.forEach(function (rank) {
+			switch (containsNTimes(handArray, rank)) {
+				case 2: 
+					pairFound += 1;
+					break;
+				case 3:
+					tripsFound += 1;
+					break;
+				case 4:
+					quadsFound += 1;
+				default:
+					result = "high card";
+			} 
+		});
+		if (quadsFound === 1) {
+			result = "You have quads! Congrats!";
+		} else if (pairFound === 1 && tripsFound === 1) {
+			result = "You have a full house!";
+		} else if (tripsFound === 1) {
+			result = "You have trips!";
+		} else if (pairFound === 2) {
+			result = "You have two-pair!";
+		} else if (pairFound === 1) {
+			result = "You have a pair!";
+		} else {
+			result = "You have nothing!";
+		}
+		return result;
+	}
+};
+
+$(document).ready(main);
