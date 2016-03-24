93b903c5f408e83e3250ea6cced53dab2d6a3013
Hsin Chih Wang <hsinwang5@gmail.com>
2016-03-24 05:56:08 -0400

    Update app.js

--- Flickr-Image-Search/app.js
+++ Flickr-Image-Search/app.js
@@ -1,30 +1,38 @@
-//optional exercise: Create a website that retrieves images from Flickr when the user 
-//submits a search term, and scroll through the images sequentially. 
-var timeoutID;
+"use strict";
+var isTimeoutRunning;
 
-var main = function() {
+function main () {
 	var url = "http://api.flickr.com/services/feeds/photos_public.gne?tags=";
 	var end = "&format=json&jsoncallback=?";
 	var input = document.querySelector("body input");
-	var button = document.querySelector("body button");
-	var images = document.querySelector("body .images");
+	var button = document.querySelectorAll("body button");
 
-	var buttonPressed = false;
-
-	button.addEventListener("click", function(event) {
-		window.clearTimeout(timeoutID);
-		var value;
-		value = input.value;
-		$.getJSON((url + input.value + end), function(flickrResponse){
+	button[0].addEventListener("click", function (event) {
+		window.clearTimeout(isTimeoutRunning);
+		$.getJSON((url + input.value + end), function (flickrResponse){
 			scrollImages(0, flickrResponse);
 		});
 	});	
+
+	input.addEventListener("keydown", function (event) {
+		if (event.which === 13) {
+			window.clearTimeout(isTimeoutRunning);
+			$.getJSON((url + input.value + end), function (flickrResponse){
+			scrollImages(0, flickrResponse);
+		});
+		}
+	})
+
+	button[1].addEventListener("click", function () {
+		window.clearTimeout(isTimeoutRunning);
+	});
 };
 
-var scrollImages = function(cycle, obj) {
+function scrollImages (cycle, obj) {
 	var imgsrc = document.querySelector("body img");
 	imgsrc.setAttribute("src", obj.items[cycle].media.m);	
-	timeoutID = setTimeout(function() {
+	isTimeoutRunning = setTimeout(function() {
+		console.log(isTimeoutRunning);
 		cycle += 1;
 		if (cycle === obj.items.length) {
 			console.log("end reached");
@@ -32,7 +40,10 @@ var scrollImages = function(cycle, obj) {
 			return;
 		}
 		scrollImages(cycle, obj);
-	}, 2000)
+	}, 500)
 };
 
+//---------------------------------------------------------------------------------------------------------------
+
 $(document).ready(main);
+
