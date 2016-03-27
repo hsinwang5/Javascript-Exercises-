97ac068c144edccbbbc2efc33d7e64566da4a62c
Hsin Wang <hsinwang5@gmail.com>
2016-03-27 17:51:21 -0400

    Add basic drag and drop JS functionality

--- Flickr-Image-Search/javascripts/app.js
+++ Flickr-Image-Search/javascripts/app.js
@@ -28,20 +28,23 @@ function main () {
 	});
 
 	//Draggable Image using custom Javascript code
-	var dragImg = document.querySelector(".images img");
-	var dragging = false;
-	dragImg.addEventListener("mousedown", function () {
+		var dragImg = document.querySelector(".images img");
+		var dragging = false;
+	
+	dragImg.addEventListener("mousedown", function (event) {
+		event.stopPropagation();
 		console.log("mousedown detected " + dragImg.clientHeight);
 		dragImg.setAttribute("id", "drag");
 		dragging = true;
+
 	});
-	dragImg.addEventListener("mousemove", function (event) {
+	document.addEventListener("mousemove", function (event) {
 		if (dragging) {
 			dragImg.style.top = (event.clientY - dragImg.clientHeight/2) + "px";
 			dragImg.style.left = (event.clientX - dragImg.clientWidth/2) + "px";
 		}
 	})
-	dragImg.addEventListener("mouseup", function () {
+	document.addEventListener("mouseup", function () {
 		console.log("mouseup detected");
 		dragging = false;
 		//reset position if not in clipboard
@@ -49,7 +52,26 @@ function main () {
 		dragImg.style.top = "";
 		dragImg.style.left = "";
 	});
-};
+
+	//Clipboard behavior
+	var clipboard = document.querySelector(".clipboard");
+	clipboard.addEventListener("mouseenter", function () {
+		if (dragging) {
+			console.log("mouse entered clipboard!");
+			clipboard.style.backgroundColor = "red";
+		}
+	});
+	clipboard.addEventListener("mouseleave", function () {
+		if (dragging) {
+			clipboard.style.backgroundColor = "";
+		}
+	});
+	clipboard.addEventListener("mouseup", function () {
+		if (dragging) {
+			clipboard.style.backgroundColor = "";
+		}
+	});
+}
 
 function scrollImages (cycle, obj, time) {
 	var imgsrc = document.querySelector("body img");
@@ -66,7 +88,9 @@ function scrollImages (cycle, obj, time) {
 	}, (time*1000))
 };
 
-/*function allowDrop (ev) {
+/*
+
+function allowDrop (ev) {
 	ev.preventDefault ();
 }
 
@@ -89,7 +113,17 @@ function onDrop (ev) {
 	} else {
 		eraseImg.setAttribute("src", "");
 	}
-}*/
+}
+
+function logger () {
+		setTimeout(function () {
+		console.log(dragging);
+		logger();
+		}, 1000);
+	}
+	logger();
+
+*/
 
 //---------------------------------------------------------------------------------------------------------------
 
